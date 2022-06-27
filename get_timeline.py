11f9ca762f94382a7a5f0a1d812f89f424b15482
Ben Ko <ben.k@wustl.edu>
2022-06-27 10:55:46 -0500

    append time, basic stats

--- get_timeline.py
+++ get_timeline.py
@@ -3,9 +3,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-import requests
 import json 
-import time
 import os
 
 options = webdriver.ChromeOptions()
@@ -23,22 +21,40 @@ password.send_keys("qJ7C64Gb!CDqBsu")
 driver.find_element(By.XPATH, "//*[@id='new_user']/input[2]").click()
 
 def get_dates(question_id):
-  r = requests.get(f"https://www.gjopen.com/questions/{question_id}")
-  soup = BeautifulSoup(r.content, 'html.parser')
-  date = soup.find("div", class_="question-openclose").find_all("span")
-  open_date = date[0].get("data-localizable-timestamp")
-  close_date = date[0].get("data-localizable-timestamp")
+  link_template = f"https://www.gjopen.com/questions/{question_id}"
+  driver.get(link_template)
+  soup = BeautifulSoup(driver.page_source, 'html.parser')
+  date = soup.find("div", class_="question-openclose").find_all("small")
+  open_date = date[0].find("span").get("data-localizable-timestamp")
+  close_date = date[1].find("span").get("data-localizable-timestamp")
   return open_date, close_date
 
-def get_all_dates(question_ids_file, path):
+
+def append_dates(question_ids_file, path):
   with open(question_ids_file, "r") as f:
     for line in f.readlines():
-      data = get_dates(int(line))
-      name = f"{path}date_{int(line)}.json"
-      with open(name, "w") as outfile:  
-        json.dump(data, outfile) 
+      open_date, close_date = get_dates(int(line))
+      data_file = f"{path}question_{int(line)}.json"
+      with open(data_file) as append_file:  
+        jfile = json.load(append_file)
+      # jfile.append({
+      #     "open_date": open_date,
+      #     "close_date": close_date
+      # })
+        jfile["open_date"] = open_date
+        jfile["close_date"] = close_date
+      with open(data_file, 'w') as json_file:
+        json.dump(jfile, json_file)
+        # print(jfile)
 
-id_file = os.path.expanduser("~/Desktop/id_file_modified.txt")
-save_path = os.path.expanduser("~/Desktop/data/")
 
-get_all_dates(id_file, save_path)
+# id_file = os.path.expanduser("~/Desktop/id_file_modified.txt")
+# save_path = os.path.expanduser("~/Desktop/data/")
+
+# testing with question_2418.json
+
+test_data = os.path.expanduser("~/Desktop/test_id.txt")
+test_path = os.path.expanduser("~/Desktop/")
+
+append_dates(test_data, test_path)
+
--- prediction_stack.py
+++ prediction_stack.py
@@ -7,14 +7,45 @@ def df(data_file):
   # data_file = f"question_{question_id}.json"
   jdata = json.load(open(data_file))
   dataframe = pd.DataFrame.from_dict(jdata, orient='index')
-  predictions = dataframe.loc["preds", :]
+  return dataframe
+
+def get_prediction_stack(data_file):
+  dataframe = df(data_file)
   prediction_stack = []
-  for prediction in predictions:
+  for prediction in dataframe.loc["preds", :]:
     prediction_stack = np.row_stack(prediction)
   prediction_stack = pd.DataFrame(prediction_stack)
-  prediction_stack = prediction_stack.rename(columns={0: "user_id", 1: "date_time", 2:"pred", 3:"text"})
+  prediction_stack = prediction_stack.rename(columns={0: "user_id", 1: "date_time", 2:"pred", 3:"text", 4: "open_date", 5: "close_date"})
+  return prediction_stack
 
+def basic_stats(data_file):
+  table = get_prediction_stack(data_file)
+  forecast_count = len(table.index)
+  written_justification_count = 0
+  for text_input in pd.DataFrame(table.loc[:, "text"]):
+    if text_input != None:
+      written_justification_count += 1
+  print(forecast_count, written_justification_count)
+  return forecast_count, written_justification_count
 
+def loop_all_data(id_file, path):
+  total_forecast_count = 0
+  total_written_justification_count = 0
+  with open(id_file, "r") as f:
+    for line in f.readlines():
+      data_file = f"{path}question_{int(line)}.json"
+      forecast_count, written_justification_count = basic_stats(data_file)
+      total_forecast_count += forecast_count
+      total_written_justification_count += written_justification_count
+  print(total_forecast_count, total_written_justification_count)
+  return {
+    "total_forecasts": total_forecast_count,
+    "total_textual_justification": total_written_justification_count
+  }
+      
+test_path = os.path.expanduser("~/Desktop/")
+test_id_file = os.path.expanduser("~/Desktop/test_id.txt")
+test_data_file = os.path.expanduser("~/Desktop/question_2418.json")
 
-test_path = os.path.expanduser("~/Desktop/question_2418.json")
-df(test_path)
+basic_stats(test_data_file)
+loop_all_data(test_id_file, test_path)
