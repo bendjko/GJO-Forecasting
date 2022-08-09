9208eafb0a431063be4a974aeb744f2743829641
Ben Ko <ben.k@wustl.edu>
2022-08-08 20:10:39 -0500

    updates

--- model_building.py
+++ model_building.py
@@ -70,45 +70,58 @@ def binary_option_forecast_representation(day, individual_prediction):
 
 def question_representation(dataframe):
     clf = Bert_Model()
-    dim_matcher = [0, 0]
+    place_holder = np.array([[0]])
     output = clf(preprocess(dataframe.loc["title"][0])).detach().numpy()
-    return torch.from_numpy(np.concatenate((output, dim_matcher), axis=None))
-
+    return torch.from_numpy(np.append(output, [0,0]))
 def binary_option_input(question, forecast):
-    return torch.cat((question, forecast), 0)
+    return torch.from_numpy(np.vstack((question,forecast)))
+    # return torch.cat((question, forecast), 0)
+
+
+# sequence, timestep, feature
+# sequence: number of forecasts
+# timestep: 514
+# feature: 
+
+def concatenate_tensors(tensor1, tensor2):
+    return torch.vstack((tensor1, tensor2))
+
+def return_input_tensor(preds):
+    input_tensor = torch.empty(514)
+    for ind in preds.index:
+        pred = preds.iloc[ind]
+        forecast_rep = binary_option_forecast_representation(1,pred)
+        question_and_forecast = binary_option_input(question_rep, forecast_rep)
+
+        print("QUESTION_AND_FORECAST:", question_and_forecast)
+        print("SIZE:", question_and_forecast.size())
+
+        input_tensor = concatenate_tensors(input_tensor, question_and_forecast)
+
+        print("SIZE2:", input_tensor.size())
+
+    row_exclude = 1
+    input_tensor = torch.from_numpy(input_tensor)
+    input_tensor = torch.cat((input_tensor[:row_exclude],input_tensor[row_exclude+1:]))
+
+    print("INPUT_TENSOR:", input_tensor)
+    print("SIZE:", input_tensor.size())
+
+     #[x, 514] sized 2d  tensor
+    return input_tensor
+
 
 def call_questions(dataframe):
     correct_answer, possible_answers = correct_possible_answer(dataframe)
     prediction_stack = get_prediction_stack(dataframe)
     longest_day = prediction_stack["days_past"].max()
 
-# sequence, timestep, feature
-# sequence: number of forecasts
-# timestep: 516
-# feature: 
-
-def concatenate_tensors(tensor1, tensor2):
-    # tensor1 = tensor1.detach().numpy()
-    # tensor2=tensor2.detach().numpy()
-    return np.vstack((tensor1, tensor2))
-
-def return_input_tensor(preds):
-    input_tensor = torch.empty(516)
-    for ind in preds.index:
-        pred = preds.iloc[ind]
-        forecast_rep = binary_option_forecast_representation(1,pred)
-        question_and_forecast = binary_option_input(question_rep, forecast_rep)
-        input_tensor = concatenate_tensors(input_tensor, question_and_forecast)
-        print(input_tensor)
-    return torch.from_numpy(input_tensor)
-
 test_file = os.path.expanduser("~/Desktop/question_1951.json")
 dataframe = df(test_file)
 question_rep = question_representation(dataframe)
+print(question_rep)
 preds = get_prediction_stack(dataframe)
 input_tensor = return_input_tensor(preds)
-
-print("input_tensor:", input_tensor)
 
 # lstmlayer = LSTMlayer()
 # print(lstmlayer(input_tensor))
